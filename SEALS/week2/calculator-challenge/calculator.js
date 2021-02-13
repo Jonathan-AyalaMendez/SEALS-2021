6e9687181f1aa596bee7fe883a4b7215f6e2133a
Jonathan-AyalaMendez <66805856+Jonathan-AyalaMendez@users.noreply.github.com>
2021-02-13 01:32:21 -0800

    Add files via upload

--- SEALS/week2/calculator-challenge/calculator.js
+++ SEALS/week2/calculator-challenge/calculator.js
@@ -1,164 +1,103 @@
-const calculator = document.querySelector(".calculator")
-const keys = calculator.querySelector(".calculator__keys")
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
 
-keys.addEventListener("click", e => {
- if (e.target.matches("button")) {
-   // Do something
- }
-})
+function inputDigit(digit) {
+  const { displayValue, waitingForSecondOperand } = calculator;
 
-const key = e.target
-const action = key.dataset.action
-
-//number keys
-if (!action) {
-    console.log('number key!')
-  }
-
-//arithmatic keys
-if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-  ) {
-    console.log('operator key!')
-  }
-
-//decimal key
-  if (action === 'decimal') {
-    console.log('decimal key!')
-  }
-  
-// clear key
-  if (action === 'clear') {
-    console.log('clear key!')
-  }
-  
-//equal key
-  if (action === 'calculate') {
-    console.log('equal key!')
-  }
-
-        ///Calculator functionality\\\
-//get two variables
-const display = document.querySelector('.calculator__display')
-
-keys.addEventListener('click', e => {
-  if (e.target.matches('button')) {
-    const key = e.target
-    const action = key.dataset.action
-    const keyContent = key.textContent
-    const displayedNum = display.textContent
-    // ...
-  }
-})
-
-//replace displayed 0 w/ number
-if (!action) {
-    if (displayedNum === '0') {
-      display.textContent = keyContent
-    }
-  }
-
-//display nonzero
-if (!action) {
-    if (displayedNum === '0') {
-      display.textContent = keyContent
-    } else {
-      display.textContent = displayedNum + keyContent
-    }
-  }
-
-//connect decimal to number
-if (action === 'decimal') {
-    display.textContent = displayedNum + '.'
-  }
-
-//highlight clicked arithmatic key
-if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-  ) {
-    key.classList.add('is-depressed')
-  }
-
-//remove highligh
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-      const key = e.target
-      // ...
-      
-      // Remove .is-depressed class from all keys
-      Array.from(key.parentNode.children)
-        .forEach(k => k.classList.remove('is-depressed'))
-    }
-  })
-
-//update display
-const calculator = document.querySelector('.calculator')
-// ...
-
-keys.addEventListener('click', e => {
-  if (e.target.matches('button')) {
-    // ...
-    
-    if (
-      action === 'add' ||
-      action === 'subtract' ||
-      action === 'multiply' ||
-      action === 'divide'
-    ) {
-      key.classList.add('is-depressed')
-      // Add custom attribute
-      calculator.dataset.previousKeyType = 'operator'
-    }
-  }
-})
-
-//replace number being displayed w/ new number
-const previousKeyType = calculator.dataset.previousKeyType
-
-if (!action) {
-  if (displayedNum === '0' || previousKeyType === 'operator') {
-    display.textContent = keyContent
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
   } else {
-    display.textContent = displayedNum + keyContent
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
   }
 }
 
-if (action === 'calculate') {
-    const secondValue = displayedNum
-    // ...
+function inputDecimal(dot) {
+  // If the `displayValue` does not contain a decimal point
+  if (!calculator.displayValue.includes(dot)) {
+    // Append the decimal point
+    calculator.displayValue += dot;
+  }
+}
+
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator
+  const inputValue = parseFloat(displayValue);
+
+  if (operator && calculator.waitingForSecondOperand)  {
+    calculator.operator = nextOperator;
+    return;
   }
 
-//store calculator values
-if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-  ) {
-    // ...
-    calculator.dataset.firstValue = displayedNum
-    calculator.dataset.operator = action
+  if (firstOperand == null) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const currentValue = firstOperand || 0;
+    const result = performCalculation[operator](currentValue, inputValue);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
   }
 
-//calculation
-const calculate = (n1, operator, n2) => {
-    let result = ''
-    
-    if (operator === 'add') {
-      result = parseFloat(n1) + parseFloat(n2)
-    } else if (operator === 'subtract') {
-      result = parseFloat(n1) - parseFloat(n2)
-    } else if (operator === 'multiply') {
-      result = parseFloat(n1) * parseFloat(n2)
-    } else if (operator === 'divide') {
-      result = parseFloat(n1) / parseFloat(n2)
-    }
-    
-    return result
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+}
+
+const performCalculation = {
+  '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+
+  '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+
+  '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+
+  '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+
+  '=': (firstOperand, secondOperand) => secondOperand
+};
+
+function resetCalculator() {
+  calculator.displayValue = '0';
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+}
+
+function updateDisplay() {
+  const display = document.querySelector('.calculator-screen');
+  display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+  const { target } = event;
+  if (!target.matches('button')) {
+    return;
   }
+
+  if (target.classList.contains('operator')) {
+    handleOperator(target.value);
+		updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    inputDecimal(target.value);
+		updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    resetCalculator();
+		updateDisplay();
+    return;
+  }
+
+  inputDigit(target.value);
+  updateDisplay();
+});
