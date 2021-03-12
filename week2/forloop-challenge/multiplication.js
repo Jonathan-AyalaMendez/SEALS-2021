5a5c1c0b441cc9bbd5e49506f6514baa935a6c8c
TXT-12 <txt-12@MacBook-Pro.attlocal.net>
2021-03-11 20:37:25 -0800

    update all

---
+++ week2/forloop-challenge/multiplication.js
@@ -0,0 +1,5 @@
+let originalNumber = 2;
+for (let i = 0 ;i <= 10; i=i+1) {
+    let multiple = originalNumber * i;
+    console.log(multiple)
+}
---
+++ week2/npm-challenge/oneliner.js
@@ -0,0 +1,43 @@
+
+
+var oneLinerJoke = require('one-liner-joke');
+
+
+/*
+The variable getRandomJoke will contain a random joke with a format:
+{"body":"Artificial intelligence is no match for natural stupidity.","tags":["intelligence","stupid"]}
+*/
+const oneliner = {body:””, tags: [‘’]}
+
+var getRandomJoke = oneLinerJoke.getRandomJoke();
+console.log(getRandomJoke)
+
+function oneLiner() {
+    alert(oneLiner.body);
+  }
+
+/*
+One can add exclusion filter for the jokes tags
+default is ['racist', 'dirty', 'sex']
+
+var getRandomJoke = oneLinerJoke.getRandomJoke({
+    'exclude_tags': ['dirty', 'racist', 'marriage']
+  });
+console.log(getRandomJoke)
+
+
+/*
+The variable getRandomJoke will contain a random joke with a tag and with a format:
+{"body":"Artificial intelligence is no match for natural stupidity.","tags":["intelligence","stupid"]}
+
+
+var getRandomJokeWithTag = oneLinerJoke.getRandomJokeWithTag('stupid');
+console.log(getRandomJokeWithTag)
+
+/*
+One can add exclusion filter for the jokes tags
+default is ['racist', 'dirty', 'sex']
+var getRandomJoke = oneLinerJoke.getRandomJokeWithTag('stupid', {
+    'exclude_tags': ['dirty', 'racist', 'marriage']
+  });
+  console.log(getRandomJoke) */
